064f5e21902c5962501dc7250c4ea7da41637df7
Edlawit-H <edlawithuluwork82@gmail.com>
2025-08-03 11:10:01 +0300

    Update on search

--- github-user-search/src/services/githubService.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export async function fetchUserData(username) {
-  const url = `https://api.github.com/users/${username}`;
-  const response = await axios.get(url);
-  return response.data;
-}

269f579d708de8f5317ef25c807456d9aa39ba86
Edlawit-H <edlawithuluwork82@gmail.com>
2025-08-03 10:33:17 +0300

    Initial GitHub user search setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>GitHub User Search</h1>
+      <p>Start searching for GitHub users!</p>
+    </div>
+  );
+}
+
+export default App;

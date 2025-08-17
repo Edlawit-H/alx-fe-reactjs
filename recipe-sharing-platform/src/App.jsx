6992dc9e2185ef198ee8faf9b28aa1adec09d51d
Edlawit-H <edlawithuluwork82@gmail.com>
2025-08-17 21:00:39 +0300

    setup React project with Tailwind CSS

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,7 @@
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <h1 className="text-blue-500 text-3xl font-bold">Hello, Recipes!</h1>
-  )
+  return <HomePage />;
 }
+
 export default App;

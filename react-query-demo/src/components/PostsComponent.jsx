d8ace916bbf7b69aff18409e2df9e80031529703
Edlawit-H <edlawithuluwork82@gmail.com>
2025-08-29 16:45:18 +0300

    add fetchPosts function and integrate with useQuery while keeping error handling and query options

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const fetchPosts = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return res.data;
+};
+
 export default function PostsComponent() {
   const { data, isLoading, isError, error, refetch } = useQuery(
     ["posts"],
-    () =>
-      axios
-        .get("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => res.data),
+    fetchPosts,
     {
       cacheTime: 1000 * 60 * 5, // 5 minutes
       staleTime: 1000 * 60 * 1, // 1 minute
